60927ecd39aa471c5ecd358f5beb0d40367bd73e
hopeogbons <hopeogbons@gmail.com>
2019-02-17 23:05:06 +0100

    add a template path

--- annotate/settings.py
+++ annotate/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Template path
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'annotate', 'frontend', 'build',)
 
 
 # Quick-start development settings - unsuitable for production
