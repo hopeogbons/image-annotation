4d3a646a4ab6172b67b9d34722ef078206c948c0
hopeogbons <hopeogbons@gmail.com>
2019-02-18 00:22:58 +0100

    add images view

--- annotate/backend/views.py
+++ annotate/backend/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from annotate.backend.models import Image
+from annotate.backend.serializers import ImageSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all().order_by('-date_created')
+    serializer_class = ImageSerializer
