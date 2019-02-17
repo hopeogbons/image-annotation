59e1dc8e9084a9408b2554bd019e984a956c1706
hopeogbons <hopeogbons@gmail.com>
2019-02-18 00:00:27 +0100

    add images model

--- annotate/backend/models.py
+++ annotate/backend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    desc = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_deleted = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
