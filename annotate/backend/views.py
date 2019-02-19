13d2739d64df9420bf3e98de917eebfebe3d3181
hopeogbons <hopeogbons@gmail.com>
2019-02-19 03:43:22 +0100

    add Annotation view

--- annotate/backend/views.py
+++ annotate/backend/views.py
@@ -1,8 +1,13 @@
-from annotate.backend.models import Image
-from annotate.backend.serializers import ImageSerializer
+from annotate.backend.models import Image, Annotation
+from annotate.backend.serializers import ImageSerializer, AnnotationSerializer
 from rest_framework import viewsets
 
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all().order_by('-date_created')
     serializer_class = ImageSerializer
+
+
+class AnnotationViewSet(viewsets.ModelViewSet):
+    queryset = Annotation.objects.all().order_by('-date_created')
+    serializer_class = AnnotationSerializer
