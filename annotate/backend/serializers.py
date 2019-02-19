4cda4a602daa985dd771f48f4467d36480893843
hopeogbons <hopeogbons@gmail.com>
2019-02-19 03:42:58 +0100

    add Annotation serializer

--- annotate/backend/serializers.py
+++ annotate/backend/serializers.py
@@ -3,7 +3,19 @@ from rest_framework import serializers
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    count_annotations = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_count_annotations(obj):
+        return obj.annotations.count()
 
     class Meta:
         model = Image
-        fields = ('id', 'title', 'desc', 'date_created', 'date_updated', 'date_deleted')
+        fields = ('id', 'title', 'desc', 'file_path', 'count_annotations', 'date_created', 'date_updated',
+                  'date_deleted')
+
+
+class AnnotationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ('id', 'image', 'details', 'date_created', 'date_updated', 'date_deleted')
