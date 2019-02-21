7c87f3c1868195654c39aa1537dd4956b694f29b
hopeogbons <hopeogbons@gmail.com>
2019-02-21 09:52:12 +0100

    add django url filter

--- annotate/backend/views.py
+++ annotate/backend/views.py
@@ -1,5 +1,6 @@
 from annotate.backend.models import Image, Annotation
 from annotate.backend.serializers import ImageSerializer, AnnotationSerializer
+from url_filter.integrations.drf import DjangoFilterBackend
 from rest_framework import viewsets
 
 
@@ -11,3 +12,5 @@ class ImageViewSet(viewsets.ModelViewSet):
 class AnnotationViewSet(viewsets.ModelViewSet):
     queryset = Annotation.objects.all().order_by('-date_created')
     serializer_class = AnnotationSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['image']
