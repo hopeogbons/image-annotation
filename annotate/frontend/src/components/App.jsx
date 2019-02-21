b1ac5bf333c75ccb22c7f04064c3bfcbd852881f
hopeogbons <hopeogbons@gmail.com>
2019-02-21 09:56:13 +0100

    add viewImage route

--- annotate/frontend/src/components/App.jsx
+++ annotate/frontend/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ImageList from '../pages/ImageList';
+import ImageView from '../pages/ImageView';
 
 export default class App extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends Component {
       <div>
         <Switch>
           <Route exact path="/" component={ ImageList } />
+          <Route path="/images/annotate/:id" component={ ImageView } />
         </Switch>
       </div>
     )
