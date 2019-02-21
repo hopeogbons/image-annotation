b6cf242b2d5f7c4bc5b9e38885d798a1380dbba0
hopeogbons <hopeogbons@gmail.com>
2019-02-21 10:02:11 +0100

    refactor index reducer

--- annotate/frontend/src/reducers/index.jsx
+++ annotate/frontend/src/reducers/index.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import image from './image';
+import annotation from './annotation';
 
 export default (history) => combineReducers({
   router: connectRouter(history),
-  image
+  image,
+  annotation
 })
