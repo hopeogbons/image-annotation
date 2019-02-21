279a78b4617dc50e6607b09ce93c3039c76bff89
hopeogbons <hopeogbons@gmail.com>
2019-02-21 09:54:46 +0100

    implement view image actions

--- annotate/frontend/src/actions/images.jsx
+++ annotate/frontend/src/actions/images.jsx
@@ -6,13 +6,16 @@ export const FETCH_IMAGE_SUCCESS = 'FETCH_IMAGE_SUCCESS';
 export const FETCH_IMAGE_FAILURE = 'FETCH_IMAGE_FAILURE';
 export const FETCH_IMAGE_RESET = 'FETCH_IMAGE_RESET';
 
+// view
+export const VIEW_IMAGE_REQUEST = 'VIEW_IMAGE_REQUEST';
+export const VIEW_IMAGE_SUCCESS = 'VIEW_IMAGE_SUCCESS';
+export const VIEW_IMAGE_FAILURE = 'VIEW_IMAGE_FAILURE';
+export const VIEW_IMAGE_RESET = 'VIEW_IMAGE_RESET';
+
 const ROOT_URL = 'http://localhost:8000/api/v1';
 
 export function fetchImageRequest() {
   const payload = axios({
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
     method: 'get',
     url: `${ROOT_URL}/images/`
   })
@@ -51,6 +54,49 @@ export function fetchImageFailure(error) {
 export function fetchImageReset() {
   return {
     type: FETCH_IMAGE_RESET
+  };
+}
+
+export function viewImageRequest(id) {
+  const payload = axios({
+    method: 'get',
+    url: `${ROOT_URL}/images/${id}/`,
+  })
+    .then(res => {
+      const request = { loading: true };
+      const {data, status} = res;
+      if (data && status === 200) {
+        request.data = data;
+      } else {
+        request.error = 'Unable to view image at the moment. Please, try again later.';
+      }
+      return request
+    })
+
+  return {
+    type: VIEW_IMAGE_REQUEST,
+    payload: payload
+  }
+}
+export function viewImageSuccess(data) {
+  const payload = { data: data };
+
+  return {
+    type: VIEW_IMAGE_SUCCESS,
+    payload: payload
+  };
+}
+export function viewImageFailure(error) {
+  const payload = { error: error };
+
+  return {
+    type: VIEW_IMAGE_FAILURE,
+    payload: payload
+  };
+}
+export function viewImageReset() {
+  return {
+    type: VIEW_IMAGE_RESET
   };
 }
 
