b456649d8a8ff106d88f221d7f13f238d6ff6243
hopeogbons <hopeogbons@gmail.com>
2019-02-21 10:01:35 +0100

    refactor image reducer

--- annotate/frontend/src/reducers/image.jsx
+++ annotate/frontend/src/reducers/image.jsx
@@ -1,9 +1,11 @@
 import {
-  FETCH_IMAGE_REQUEST, FETCH_IMAGE_SUCCESS, FETCH_IMAGE_FAILURE, FETCH_IMAGE_RESET
+  FETCH_IMAGE_REQUEST, FETCH_IMAGE_SUCCESS, FETCH_IMAGE_FAILURE, FETCH_IMAGE_RESET,
+  VIEW_IMAGE_REQUEST, VIEW_IMAGE_SUCCESS, VIEW_IMAGE_FAILURE, VIEW_IMAGE_RESET
 } from '../actions/images';
 
 const INITIAL_STATE = {
-  fetchedImage: { data: [], error: null, loading: false }
+  fetchedImage: { data: [], error: null, loading: false },
+  viewedImage: { data: null, error: null, loading: false },
 };
 
 export default function(state = INITIAL_STATE, action) {
@@ -17,6 +19,16 @@ export default function(state = INITIAL_STATE, action) {
       return { ...state, fetchedImage: { ...INITIAL_STATE.fetchedImage, ...action.payload } };
     case FETCH_IMAGE_RESET:
       return { ...state, fetchedImage: { ...INITIAL_STATE.fetchedImage } };
+
+    // view
+    case VIEW_IMAGE_REQUEST:
+      return { ...state, viewedImage: { ...INITIAL_STATE.viewedImage, ...action.payload } };
+    case VIEW_IMAGE_SUCCESS:
+      return { ...state, viewedImage: { ...INITIAL_STATE.viewedImage, ...action.payload } };
+    case VIEW_IMAGE_FAILURE:
+      return { ...state, viewedImage: { ...INITIAL_STATE.viewedImage, ...action.payload } };
+    case VIEW_IMAGE_RESET:
+      return { ...state, viewedImage: { ...INITIAL_STATE.viewedImage } };
 
     default:
       return state;
