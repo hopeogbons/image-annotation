86e94cc4f815b53a8e20b6fc4f7098c022252511
hopeogbons <hopeogbons@gmail.com>
2019-02-21 09:58:08 +0100

    refactor ImageList component

--- annotate/frontend/src/components/ImageList.jsx
+++ annotate/frontend/src/components/ImageList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export default class ImageList extends Component {
-  componentDidMount() {
+  componentWillMount() {
     this.props.fetchImages();
   }
 
@@ -9,20 +9,12 @@ export default class ImageList extends Component {
     return images.map(img => {
       return (
         <div className="col-md-4 bg-white" key={img.id}>
-          <div className="card mb-4 shadow-sm">
+          <div className="card mb-4 shadow-sm" onClick={this.props.viewImage.bind(null, img.id)} style={{cursor: 'pointer'}}>
             <img className="card-img-top" src={'/static/images/' + img.file_path} alt={img.title} />
             <div className="card-body">
               <h4>{img.title}</h4>
               <h6>{img.desc}</h6>
               <p className="card-text"><span className="badge badge-pill badge-dark">{img.count_annotations}</span> Annotations</p>
-              <div className="d-flex justify-content-between align-items-center row">
-                <div className="btn-group col">
-                  <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                  <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
-                </div>
-                <span className="text-muted col">Channel <span className="badge badge-pill badge-warning">{img.id}</span></span>
-              </div>
             </div>
           </div>
         </div>
@@ -35,7 +27,7 @@ export default class ImageList extends Component {
 
     if (loading) {
       return (
-        <div className="container">
+        <div className="container" style={{width: '1000px'}}>
           <h1>Images</h1><h3>Loading...</h3>
         </div>
       )
@@ -49,15 +41,13 @@ export default class ImageList extends Component {
       return (
         <div>
           <div className="nav-scroller bg-white shadow-sm">
-            <div className="container">
-              <div className="row py-2 px-3">
-                <button className="btn btn-info btn-sm ml-auto">+ New Image</button>
-              </div>
+            <div className="container" style={{width: '1000px'}}>
+              <div className="row py-2 px-3"><h4>A List of Beer Bottles</h4></div>
             </div>
           </div>
           <main role="main">
-            <div className="container">
-              <div className="row" style={{ margin: '50px 0 80px' }}>
+            <div className="container" style={{width: '1000px'}}>
+              <div className="row" style={{ margin: '30px 0' }}>
                 {this.renderImages(data)}
               </div>
             </div>
