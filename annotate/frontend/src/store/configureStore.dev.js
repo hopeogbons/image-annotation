7ac2e43205ff845137fb1dce41dea2761a279c09
hopeogbons <hopeogbons@gmail.com>
2019-02-21 10:02:49 +0100

    refactor dev store

--- annotate/frontend/src/store/configureStore.dev.js
+++ annotate/frontend/src/store/configureStore.dev.js
@@ -2,13 +2,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history'
 import rootReducer from '../reducers';
 import logger from '../middlewares/logger'
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 
-const initialState = {};
 const enhancers = [];
 const middleware = [thunk, promise, routerMiddleware(history), logger];
 
@@ -18,13 +17,16 @@ if (typeof devToolsExtension === 'function') {
   enhancers.push(devToolsExtension());
 }
 
-const composedEnhancers = compose(
-  applyMiddleware(...middleware),
-  ...enhancers
-);
-
-export default createStore(
-  rootReducer(history),
-  initialState,
-  composedEnhancers
-);
+export default function configureStore(preloadedState) {
+  return createStore(
+    rootReducer(history),
+    preloadedState,
+    compose(
+      applyMiddleware(
+        routerMiddleware(history),
+        ...middleware,
+        ...enhancers,
+      ),
+    ),
+  );
+}
