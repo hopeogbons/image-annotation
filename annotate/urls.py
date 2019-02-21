4e64c72aae2d8ef1d1f901aa6762b269b6d70a8a
hopeogbons <hopeogbons@gmail.com>
2019-02-21 10:05:33 +0100

    register the annotation url

--- annotate/urls.py
+++ annotate/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
-from annotate.backend.views import ImageViewSet
+from annotate.backend.views import ImageViewSet, AnnotationViewSet
 
 router = DefaultRouter()
 router.register(r'images', ImageViewSet, base_name='image')
+router.register(r'annotations', AnnotationViewSet, base_name='annotation')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
